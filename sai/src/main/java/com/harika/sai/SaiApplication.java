5a9270bf8f946060862d1506b69f1dfe1925bdda
dell <kasyapa.kvsm@gmail.com>
2017-10-08 18:25:55 +0530

    test

---
+++ sai/src/main/java/com/harika/sai/SaiApplication.java
@@ -0,0 +1,12 @@
+package com.harika.sai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiApplication.class, args);
+	}
+}
